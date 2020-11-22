9617c317de21135183f0fef129963dc2e8c5117c
narek4241 <narekghazaryan02@gmail.com>
2020-11-23 00:38:11 +0400

    added configs

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || process.env.NODE_PORT || 3001,
-  MONGO_URI: process.env.MONGO_URI || 3001,
+  MONGO_URI: process.env.MONGO_URI || `mongodb://localhost:27017/vue-meetuper`,
   NODE_ENV: process.env.NODE_ENV || `development`,
 };
