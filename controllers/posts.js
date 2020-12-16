c37b670235a24e7ed0c351fcb03db693b77330ef
narek4241 <narekghazaryan02@gmail.com>
2020-12-17 01:30:52 +0400

    added sendPost [SERVER] implementation

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,16 +1,37 @@
+const Post = require('../models/posts');
 const Thread = require('../models/threads');
 
-exports.getPosts = function(req, res) {
+exports.getPosts = function (req, res) {
   const threadId = req.query.threadId;
 
-  Post.find({'thread': threadId})
-        .populate('user')
-        .exec((errors, posts) => {
+  Post.find({ thread: threadId })
+    .populate('user')
+    .exec((errors, posts) => {
+      if (errors) {
+        return res.status(422).send({ errors });
+      }
 
+      return res.json(posts);
+    });
+};
+
+exports.sendPost = (req, res) => {
+  const postData = req.body;
+  const post = new Post(postData);
+  post.user = req.user;
+
+  post.save((errors, createdPost) => {
     if (errors) {
-      return res.status(422).send({errors});
+      res.status(422).send({ errors });
     }
 
-    return res.json(posts);
+    Thread.update(
+      { _id: createdPost.thread },
+      // #note1 we can also push an ID i.o createdPost itself (my*) opt
+      { $push: { posts: createdPost } },
+      () => {
+        res.send({ createdPost });
+      }
+    );
   });
-}
+};
